c5976b8aff2798700ef550c9fb419440dc8027b8
sunyujie <yjsuncr@isoftstone.com>
2025-12-21 20:27:57 +0800

    refactor: 更新 默认偏好设置，禁用语言切换和时区选项，简化工具栏列表

--- packages/effects/layouts/src/authentication/toolbar.vue
+++ packages/effects/layouts/src/authentication/toolbar.vue
@@ -21,7 +21,7 @@ defineOptions({
 });
 
 const props = withDefaults(defineProps<Props>(), {
-  toolbarList: () => ['color', 'language', 'layout', 'theme'],
+  toolbarList: () => ['color', 'layout', 'theme'],
 });
 
 const showColor = computed(() => props.toolbarList.includes('color'));
